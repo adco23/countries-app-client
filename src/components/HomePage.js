cdf9fec6e62056b7bdc209c2e77e55fa585a9962
adco23 <adco2393@gmail.com>
2022-02-28 22:34:39 -0300

    Add spinner

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,6 +15,7 @@ import { Link } from "react-router-dom";
 import Card from "./Card";
 import Pagination from "./Pagination";
 import SearchBar from "./SearchBar";
+import Spinner from "./Spinner";
 
 // Styles ---------------------------
 import style from "./HomePage.module.css";
@@ -179,7 +180,7 @@ const HomePage = () => {
       <div className={style.cards_container}>
         {
           countries.length === 0 ?
-          <p>Loading...</p> :
+          <Spinner/> :
           currentCountries?.map((country) => (
             <Link
               to={`home/country/${country.id}`}
---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div class="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Spinner;
